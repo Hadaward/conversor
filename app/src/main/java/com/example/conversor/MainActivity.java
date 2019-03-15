3928441a951701ab4ed1c79854405539a0909bb3
bevilacqua <edu_bevilacqua@outlook.com>
2019-03-15 10:04:22 -0300

    criação ConversorActivity.java e criação de uma splash screen na mainActivity.java

--- app/src/main/java/com/example/conversor/MainActivity.java
+++ app/src/main/java/com/example/conversor/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.conversor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,ConversaoActivity.class);
+                startActivity(intent);
+            }
+        },6000);
     }
 }
