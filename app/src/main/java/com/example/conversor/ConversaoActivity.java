e88393f8238ce49963ffc9e3cf680e3825a19827
bevilacqua <edu_bevilacqua@outlook.com>
2019-03-15 11:48:35 -0300

    criação ConversorActivity.java e criação de uma splash screen na mainActivity.java

--- app/src/main/java/com/example/conversor/ConversaoActivity.java
+++ app/src/main/java/com/example/conversor/ConversaoActivity.java
@@ -1,13 +1,26 @@
 package com.example.conversor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ConversaoActivity extends AppCompatActivity {
-
+    public double realToDolar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversao);
+
     }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this,Resultado.class);
+        EditText real = (EditText) findViewById(R.id.realToDolar);
+        realToDolar = Float.valueOf(real.getText().toString())/3.83;
+        intent.putExtra("dolar", realToDolar);
+        startActivity(intent);
+
+    };
 }
